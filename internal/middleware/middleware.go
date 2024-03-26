1aa6b176b5da90d0832cf00c37c074fdf952c457
Väinö Jalonen <vaino.jalonen@reaktor.com>
2024-03-26 18:12:33 +0200

    Fix content type header by adding a semicolon

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -58,7 +58,7 @@ func CSPMiddleware(next http.Handler) http.Handler {
 
 func TextHTMLMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html charset=utf-8")
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		next.ServeHTTP(w, r)
 	})
 }
